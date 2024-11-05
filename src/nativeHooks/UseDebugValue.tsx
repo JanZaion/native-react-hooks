cdbda1ecdb0b3d5b2f8248b1c4159db975f9c68d
Jan Zaion <62064996+JanZaion@users.noreply.github.com>
2024-11-05 23:16:13 +0100

    feat: add useDebugValue

--- src/nativeHooks/UseDebugValue.tsx
+++ src/nativeHooks/UseDebugValue.tsx
@@ -1,3 +1,30 @@
+import { useState, useEffect, useDebugValue } from "react";
+
+// useDebugValue is a custom hook that allows to
+// display a label for custom hooks in React DevTools.
+const useOnlineStatus = () => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useDebugValue(isOnline ? "Online 🟢" : "Offline 🔴");
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return isOnline;
+};
+
 export const UseDebugValue = () => {
-  return <div>UseDebugValue</div>;
+  const isOnline = useOnlineStatus();
+
+  return <div>User is: {isOnline ? "Online" : "Offline"}</div>;
 };
