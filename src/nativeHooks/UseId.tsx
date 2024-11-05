d5dfc20013a35859e4d153c98fa802091a5b5650
Jan Zaion <62064996+JanZaion@users.noreply.github.com>
2024-11-05 23:06:21 +0100

    feat: add useId

--- src/nativeHooks/UseId.tsx
+++ src/nativeHooks/UseId.tsx
@@ -1,3 +1,34 @@
+import { useId, useState } from "react";
+
+// Generates a unique id that is consistent across renders.
 export const UseId = () => {
-  return <div>UseId</div>;
+  const id = useId();
+  const [color, setColor] = useState("#000000");
+
+  const generateRandomColor = () => {
+    const randomColor =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setColor(randomColor);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1
+          style={{
+            padding: "20px",
+            backgroundColor: color,
+            color: "white",
+            marginBottom: "10px",
+          }}
+        >
+          id: {id}
+        </h1>
+        <p>Current color: {color}</p>
+      </div>
+      <button onClick={generateRandomColor}>
+        Generate New Color
+      </button>
+    </div>
+  );
 };
