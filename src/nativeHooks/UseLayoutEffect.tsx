2baf05c6def821c18afc3d97b0e8282da3c22949
Jan Zaion <62064996+JanZaion@users.noreply.github.com>
2024-11-08 14:12:55 +0100

    feat: add useLayout effect

--- src/nativeHooks/UseLayoutEffect.tsx
+++ src/nativeHooks/UseLayoutEffect.tsx
@@ -1,3 +1,48 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
+// Like useEffect, but before the browser paints.
 export const UseLayoutEffect = () => {
-  return <div>UseLayoutEffect</div>;
+  const divRef = useRef<HTMLDivElement>(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useLayoutEffect(() => {
+    const handleScroll = () => {
+      if (divRef.current) {
+        setScrollPosition(divRef.current.scrollTop);
+      }
+    };
+
+    const div = divRef.current;
+    if (div) {
+      div.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (div) {
+        div.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <div
+        ref={divRef}
+        style={{
+          padding: "10px",
+          marginTop: "20px",
+          height: "300px",
+          overflowY: "scroll",
+          backgroundColor: "hotpink",
+        }}
+      >
+        <div style={{ height: "800px" }}>
+          <h1>Scroll this div to see the scroll position update.</h1>
+        </div>
+      </div>
+      <p>
+        The scroll position of the above div is: {scrollPosition}px
+      </p>
+    </div>
+  );
 };
